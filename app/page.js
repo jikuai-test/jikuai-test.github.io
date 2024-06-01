1229ec11dbe8e6f860b1e94cc32f79c37d897029
Ji Kuai <rk427454171@gmail.com>
2024-06-01 21:29:20 +0800

    update

--- app/page.js
+++ app/page.js
@@ -7,6 +7,9 @@ export default function Home() {
         justifyContent: "center",
         alignItems: "center",
       }}>
+        <h1>
+          你有認真聽前面組別的報告嗎 :)
+        </h1>
         <img src="/demo.gif" alt="Demo video" style={{
           top: "50vh"
         }} />
